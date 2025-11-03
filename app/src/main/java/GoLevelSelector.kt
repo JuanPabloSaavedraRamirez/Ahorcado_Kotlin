93d5fe0ec28973aa90f3d242f61466c7c544302b
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2025-11-03 21:49:36 +0100

    Main menu
    add:
    Main menu logic
    Go to level selector logic
    Main menu layout

--- app/src/main/java/GoLevelSelector.kt
+++ app/src/main/java/GoLevelSelector.kt
@@ -1,3 +1,11 @@
-class GoLevelSelector {
+import android.content.Context
+import android.content.Intent
+import com.example.ahorcado.LevelSelector
 
+class GoLevelSelector(private val context: Context) {
+
+    fun GoToLevelSelector() {
+        val intent = Intent(context, LevelSelector::class.java)
+        context.startActivity(intent)
+    }
 }
